37134748a9c14df1714965ee11f19f42eb596714
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-06 11:00:36 +0700

    import fetchAllUser to get users

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/userService";
+
 const TableUsers = (props) => {
-  return <>TableUsers...</>;
+  const [userlist, setUserlist] = useState([]);
+
+  useEffect(() => {
+    // call APIs
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+
+    if (res && res.data && res.data.data) {
+      setUserlist(res.data.data);
+    }
+  };
+
+  console.log(">>> Check userlist: ", userlist);
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userlist &&
+            userlist.length > 0 &&
+            userlist.map((user, index) => {
+              return (
+                <>
+                  <tr key={`user-${index}`}>
+                    <td>{user.id}</td>
+                    <td>{user.email}</td>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                  </tr>
+                </>
+              );
+            })}
+        </tbody>
+      </Table>
+    </>
+  );
 };
 
 export default TableUsers;
