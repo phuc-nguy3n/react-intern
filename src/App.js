38b0d3a8acbf0ce1c4407863308ca26a04be1b46
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-08 10:25:31 +0700

    Use Modal at App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.scss";
 import Header from "./components/Header";
+import ModalAddNew from "./components/ModalAddNew";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
 
 function App() {
+  const [isShowModelAddNew, setShowModelAddNew] = useState(false);
+
+  const handleClose = () => setShowModelAddNew(false);
+  const handleShow = () => setShowModelAddNew(true);
+
   return (
     <div className="app-container">
       <Header />
       <Container>
+        <div className="my-3 add-new">
+          <span>
+            <b>List Users:</b>
+          </span>
+          <button className="btn btn-success" onClick={handleShow}>
+            Add new user
+          </button>
+        </div>
         <TableUsers />
       </Container>
+
+      <ModalAddNew
+        isShowModelAddNew={isShowModelAddNew}
+        handleClose={handleClose}
+      />
     </div>
   );
 }
