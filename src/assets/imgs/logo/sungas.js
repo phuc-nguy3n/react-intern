e54b07a5e0b48c9bd690c9c0cee802b46751e35a
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-11 17:42:41 +0700

    Add logo

---
+++ src/assets/imgs/logo/sungas.js
@@ -0,0 +1,16 @@
+const Logo = (props) => {
+  return (
+    <>
+      <svg width="100" height="50" xmlns="http://www.w3.org/2000/svg">
+        <text x="10" y="33" font-family="Arial" font-size="24" fill="#FFA500">
+          Sun
+        </text>
+        <text x="55" y="33" font-family="Arial" font-size="24" fill="#0000FF">
+          gas
+        </text>
+      </svg>
+    </>
+  );
+};
+
+export default Logo;
