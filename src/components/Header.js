298d1eba6ca6b1989339157be6f56cddf21dc5e3
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-11 17:43:10 +0700

    Import logo at Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,24 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import SungasLogo from "../assets/imgs/logo/sungas";
 
 const Header = (props) => {
   return (
     <>
       <Navbar expand="lg" bg="light" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <SungasLogo />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+            <Nav className="me-auto" activeKey={"/users"}>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/users">Manage Users</Nav.Link>
+              <NavDropdown title="Setting" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
