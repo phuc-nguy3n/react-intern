e7fadbacce65708946097edaa04a49d9aa2a3173
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-11 17:42:10 +0700

    Maked home page component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+const Home = (props) => {
+  return (
+    <>
+      <header className="hero-section mt-3">
+        <div className="container">
+          <h1 className="display-4 text-custom">Welcome to Sungas</h1>
+          <p className="lead">
+            Your trusted partner in solar and gas solutions.
+          </p>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Home;
