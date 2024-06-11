d170a4728ae4cf1670d8b3c7bac5be61be075655
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-11 21:01:17 +0700

    Import useLocation, NavLink and use to navigate page other

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import SungasLogo from "../assets/imgs/logo/sungas";
+import { useLocation, NavLink } from "react-router-dom";
 
 const Header = (props) => {
+  const location = useLocation();
+
   return (
     <>
       <Navbar expand="lg" bg="light" className="bg-body-tertiary">
@@ -14,9 +17,15 @@ const Header = (props) => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto" activeKey={"/users"}>
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/users">Manage Users</Nav.Link>
+            <Nav className="me-auto" activeKey={location.pathname}>
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+
+              <NavLink className="nav-link" to="/users">
+                Manage Users
+              </NavLink>
+
               <NavDropdown title="Setting" id="basic-nav-dropdown">
                 <NavDropdown.Item href="/login">Login</NavDropdown.Item>
                 <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
