49e9a7be34d3b722a1af2c69757ef9dc6b80f448
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-12 15:54:00 +0700

    Import Login component at App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
-import { ToastContainer } from "react-toastify";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/users" element={<TableUsers />} />
+            <Route path="/login" element={<Login />} />
+            Login
           </Routes>
         </Container>
 
