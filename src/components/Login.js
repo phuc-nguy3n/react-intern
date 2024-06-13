78a33f1727af34e50dbdd54f6dd90efc40946954
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-13 09:59:49 +0700

    Add loading icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { loginApi } from "../services/userService";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+// Login with email (eve.holt@reqres.in)
 
 const Login = (props) => {
+  const { setIsLoggedIn } = props;
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isLoading, setIsloading] = useState(false);
+
+  const goToHomePage = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      goToHomePage();
+    }
+  }, []);
 
   const handleLogin = async () => {
     if (!email || !password) {
       toast.error("Please enter a valid email & password!");
       return;
     }
-
+    setIsloading(true);
     let res = await loginApi(email, password);
+    setIsloading(false);
     if (res && res.token) {
       localStorage.setItem("token", res.token);
+      setIsLoggedIn(true);
+      goToHomePage();
+    } else {
+      if (res && res.status === 400) {
+        toast.error(res.data.error);
+      }
+    }
+  };
+
+  const handleStatusLoginBtn = () => {
+    if (isLoading) {
+      return "loading";
+    } else {
+      if (email && password) {
+        return "active";
+      } else {
+        return "";
+      }
     }
   };
 
   return (
     <>
-      <div className="login-container  col-12 col-sm-4">
+      <div className="login-container col-12 col-sm-8 col-md-6 col-xl-4">
         <div className="title">Login</div>
         <div className="text">Email or username</div>
         <input
@@ -45,11 +82,15 @@ const Login = (props) => {
           ></i>
         </div>
         <button
-          className={email && password ? "active" : ""}
+          className={handleStatusLoginBtn()}
           disabled={email && password ? false : true}
           onClick={() => handleLogin()}
         >
-          Login
+          {isLoading ? (
+            <i class="fa-solid fa-circle-notch fa-spin"></i>
+          ) : (
+            "Login"
+          )}
         </button>
         <div className="back">
           <i className="fa-solid fa-chevron-left"></i>
