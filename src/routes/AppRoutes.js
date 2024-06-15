3622e8c8ceed3bdaad855376165283a79267d670
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-15 17:40:57 +0700

    Created AppRoutes component

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,50 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import TableUsers from "../components/TableUsers";
+import { useEffect, useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import PrivateRoute from "./PriveRoutes";
+
+const AppRoutes = (props) => {
+  const { setIsLoggedIn, setHiden } = props;
+  const { user, loginContext } = useContext(UserContext);
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    let emailTemp = localStorage.getItem("email");
+    if (token && emailTemp) {
+      setIsLoggedIn(true);
+      loginContext(emailTemp, token);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window.location.pathname === "/login") {
+      setHiden(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} setHiden={setHiden} />}
+        />
+
+        <Route
+          path="/users"
+          element={
+            <PrivateRoute>
+              <TableUsers />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRoutes;
