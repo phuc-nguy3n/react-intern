8fe0cec1c3a2990dcdb996ce054edb91a0216e63
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-16 00:08:13 +0700

    Remove Routes, Route at Private component

--- src/routes/PriveRoutes.js
+++ src/routes/PriveRoutes.js
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import Alert from "react-bootstrap/Alert";
