3d16a7d9236b79cf075cfead3fcd81006d219991
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-15 17:45:30 +0700

    Import AppRoutes component at App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import "./App.scss";
 import Header from "./components/Header";
-import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
-import Home from "./components/Home";
-import Login from "./components/Login";
+import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
-import { UserContext } from "./context/UserContext";
+import { useState } from "react";
 
 function App() {
-  const { loginContext } = useContext(UserContext);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [hiden, setHiden] = useState(false);
-
-  let token = localStorage.getItem("token");
-  let emailTemp = localStorage.getItem("email");
-
-  useEffect(() => {
-    if (token && emailTemp) {
-      setIsLoggedIn(true);
-      loginContext(emailTemp, token);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (window.location.pathname === "/login") {
-      setHiden(true);
-    }
-  }, []);
 
   return (
     <>
@@ -40,26 +19,7 @@ function App() {
           setHiden={setHiden}
         />
         <Container>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/users"
-              element={
-                token && emailTemp ? (
-                  <TableUsers />
-                ) : (
-                  <Navigate to="/login" replace />
-                )
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <Login setIsLoggedIn={setIsLoggedIn} setHiden={setHiden} />
-              }
-            />
-            Login
-          </Routes>
+          <AppRoutes setIsLoggedIn={setIsLoggedIn} setHiden={setHiden} />
         </Container>
 
         <ToastContainer
