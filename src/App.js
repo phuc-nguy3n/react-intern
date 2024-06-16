0e0f3140d6d7b2d5246de0f877c8d5b1e97b3bb3
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-16 15:42:26 +0700

    Test user by use hook useSelector

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Container from "react-bootstrap/Container";
 import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const dataUserRedux = useSelector((state) => state.user.account);
+
+  console.log(">>> Check account: ", dataUserRedux);
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [hiden, setHiden] = useState(false);
 
