ddb3b229655a6c4f81dc9d79c535c8928220ffa8
Funkii <81849786+phucnguyen-code@users.noreply.github.com>
2024-06-17 08:42:49 +0700

    Define user Reducers

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,21 +1,70 @@
-import { USER_LOGIN, USER_LOGOUT } from "../actions/userActions";
+import {
+  FETCH_USER_LOGIN,
+  FETCH_USER_ERROR,
+  FETCH_USER_SUCCESS,
+  USER_LOGOUT,
+  USER_REFRESH,
+} from "../actions/userActions";
 
 const INITIAL_STATE = {
-  account: { email: "", auth: false },
+  account: { email: "", auth: null, token: "" },
+  isLoading: false,
+  isError: false,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case USER_LOGIN:
+    case FETCH_USER_LOGIN:
       return {
         ...state,
-        count: state.count + 1,
+        isLoading: true,
+        isError: false,
+      };
+
+    case FETCH_USER_ERROR:
+      return {
+        ...state,
+        account: {
+          auth: false,
+        },
+        isLoading: false,
+        isError: true,
+      };
+
+    case FETCH_USER_SUCCESS:
+      return {
+        ...state,
+        account: {
+          email: action.data.email,
+          token: action.data.token,
+          auth: true,
+        },
+        isLoading: false,
+        isError: false,
       };
 
     case USER_LOGOUT:
       return {
         ...state,
-        count: state.count - 1,
+        account: {
+          email: "",
+          token: "",
+          auth: false,
+        },
+        isLoading: false,
+        isError: false,
+      };
+
+    case USER_REFRESH:
+      return {
+        ...state,
+        account: {
+          email: action.data.email,
+          token: action.data.token,
+          auth: true,
+        },
+        isLoading: false,
+        isError: false,
       };
 
     default:
